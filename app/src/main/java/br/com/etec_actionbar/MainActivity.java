8d76c95552dbe5929db9dc54fdb10754cc81b1d0
Omegahub <Omegahub2019@gmail.com>
2019-09-30 20:32:57 -0300

    criacao da ActionBar

---
+++ app/src/main/java/br/com/etec_actionbar/MainActivity.java
@@ -0,0 +1,22 @@
+package br.com.etec_actionbar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onProximo(View view)
+    {
+        Intent intent = new Intent(this,ActionBarActivity.class);
+        startActivity(intent);
+
+    }
+}
