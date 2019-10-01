6407dc24acc18e24298d830a261f20ed1a92603c
Omegahub <Omegahub2019@gmail.com>
2019-09-30 22:29:56 -0300

    actionBar completo

--- app/src/main/java/br/com/etec_actionbar/ActionBarActivity.java
+++ app/src/main/java/br/com/etec_actionbar/ActionBarActivity.java
@@ -2,12 +2,48 @@ package br.com.etec_actionbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ActionBarActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actions,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.action_wifi :
+                Toast.makeText(this,"Wi-fi ativado >_<",Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_bluetooth :
+                Toast.makeText(this,"Bluetooth ativado >_<",Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_sair : finish();
+            return true;
+
+            default:
+                return  super.onOptionsItemSelected(item);
+
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action_bar);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
+
+
 }
